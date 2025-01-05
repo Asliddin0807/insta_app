d49e2e821f574972cc48eef8b2ef8fe38621e71a
Asliddin0807 <komandirnuriddinov2@gmail.com>
2025-01-05 21:25:43 +0500

    bg

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <Box className="App" bg={'gray.700'}>
       <Header />
       <Box mt={10} p={5} w={"100%"}>
         <Text fontSize={25} textAlign={"center"} fontFamily={"monospace"}>
@@ -83,7 +83,7 @@ function App() {
           ""
         )}
       </Box>
-    </div>
+    </Box>
   );
 }
 
